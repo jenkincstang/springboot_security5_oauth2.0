844240cbb2f237d2277cb871a5ebd0c1734aceb6
Jenkin Tang <jenkincs@163.com>
2020-08-31 01:24:39 +0800

    [wechat-oauth2-login][jenkin] feature: add register api and test pass

--- src/main/java/cn/hinson/App.java
+++ src/main/java/cn/hinson/App.java
@@ -2,10 +2,12 @@ package cn.hinson;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class App {
-    public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
-    }
+
+  public static void main(String[] args) {
+    SpringApplication.run(App.class, args);
+  }
 }
